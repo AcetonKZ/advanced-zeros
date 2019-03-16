705cbf0a03998380ef5b38cbb0a738ea07d2f37c
AcetonKZ <ketron@mail.ru>
2019-03-17 03:25:34 +0600

    feat: getZeroCount function test implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+function factZeroCountDec(num){                 //function get count of *tail* zeros in factorial in decimal
+let n = 1;  //power count
+let result = 0;
+  while ( num/Math.pow(5,n) > 1) {
+    result=result+Math.floor(num/Math.pow(5,n));
+    n++;
+  }
+  return result;
+}  
+
+
+return factZeroCountDec(number);
 }
