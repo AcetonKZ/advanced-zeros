a1dc253080419a60479549e0976f84eb030acccb
AcetonKZ <ketron@mail.ru>
2019-03-17 03:52:17 +0600

    bag: fix bag in getZeroCount function test implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function getZerosCount(number, base) {
   
-function factZeroCountDec(num){                 //function get count of *tail* zeros in factorial in decimal
-let n = 1;  //power count
+function factZeroCountDec(num) {//function get count of *tail* zeros in factorial in decimal
+let n = 1;  //power counter
 let result = 0;
   while ( num/Math.pow(5,n) > 1) {
     result=result+Math.floor(num/Math.pow(5,n));
